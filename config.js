5f0256f21ac984250fcdd567807abaeca910d2a5
chamindulakshan20070620 <chamindulakshan20070620@gmail.com>
2025-07-25 11:32:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || ''
+    SESSION_ID: process.env.SESSION_ID || 'GQp3WZ5Z#Ay6FAUW-S9SCUbIoz_pAgtx6EE_tuG_Lc5ujj6GCh4A'
 };
